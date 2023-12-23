faa5d36a55a5bf26255d402aa55733e8d0e35d6a
Chandana <chandana.reddy.ga@gmail.com>
2023-12-23 09:15:09 +0530

    Created skeleton of homepage

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ class App extends Component {
   render() {
     return (
       <div>
+      {/* <div className="Container">
         Phone directory
       </div>
+      <button>Add</button>
+      <div>
+        <span>Name</span><br />
+        <span>Phone</span>
+      </div> */}
+      <label htmlFor="name">Name:</label>
+      <input id="name" type="text" placeholder="Type here" defaultValue={"Chandana"}></input>
+      </div>
+      
     );
   }
   
