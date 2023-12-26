229a0baeaf69fdcbb58d6e8d20ad4f525226233d
Chandana <chandana.reddy.ga@gmail.com>
2023-12-26 18:02:54 +0530

    Created skeleton part of the homepage

--- src/App.js
+++ src/App.js
@@ -11,8 +11,10 @@ class App extends Component {
         <span>Name</span><br />
         <span>Phone</span>
       </div> */}
-      <label htmlFor="name">Name:</label>
-      <input id="name" type="text" placeholder="Type here" defaultValue={"Chandana"}></input>
+      <label htmlFor="name">Userame:</label>
+      <input id="name" type="text" placeholder="Type here" defaultValue={"Chandana"}></input><br />
+      <label htmlFor="password">Password:</label>
+      <input id="password" type="password" placeholder="********" defaultValue={"!@#$%&&*"}></input>
       </div>
       
     );
